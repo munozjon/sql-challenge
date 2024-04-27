ba2db1723c3469ac88428f85af798239d46974b7
munozjon <munozjon98@gmail.com>
2024-04-27 12:12:28 -0400

    Initialized file for queries, answered first four questions

---
+++ EmployeeSQL/employee_queries.sql
@@ -0,0 +1,21 @@
+-- Question 1
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+LEFT JOIN salaries AS S ON s.emp_no = e.emp_no;
+
+-- Question 2
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986';
+
+-- Question 3
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM dept_manager AS d_m
+LEFT JOIN employees AS e ON (e.emp_no = d_m.emp_no)
+LEFT JOIN departments AS d ON (d.dept_no = d_m.dept_no);
+
+-- Question 4
+SELECT d.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS d_e
+LEFT JOIN employees AS e ON (e.emp_no = d_e.emp_no)
+LEFT JOIN departments AS d ON (d.dept_no = d_e.dept_no);
